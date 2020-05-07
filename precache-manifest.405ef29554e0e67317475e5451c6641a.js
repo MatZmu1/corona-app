98c45441224fed314e28a84c780bfcf8c5f241ca
Mateusz Zmuda <mateusz.zmuda3@gmail.com>
2020-05-07 21:21:13 +0200

    Updates

---
+++ precache-manifest.405ef29554e0e67317475e5451c6641a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15457f340c21060401395041f916b409",
+    "url": "/corona-app/index.html"
+  },
+  {
+    "revision": "98ebec91787a1852a451",
+    "url": "/corona-app/static/css/main.973c617f.chunk.css"
+  },
+  {
+    "revision": "2fb5eac5806b08f9014c",
+    "url": "/corona-app/static/js/2.0514d4c5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-app/static/js/2.0514d4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ebec91787a1852a451",
+    "url": "/corona-app/static/js/main.7baf2d0a.chunk.js"
+  },
+  {
+    "revision": "a126b372c61b4106540b",
+    "url": "/corona-app/static/js/runtime-main.68b8ed65.js"
+  }
+]);
